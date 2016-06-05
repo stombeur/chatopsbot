3c3feec8233fe90977937644d96a8b3ff62c04b8
Stephane Tombeur <stephane@hephaestus.be>
2016-06-05 23:41:32 +0200

    testconsole

--- src/TestConsole/Program.cs
+++ src/TestConsole/Program.cs
@@ -15,13 +15,21 @@ namespace TestConsole
     {
         static void Main(string[] args)
         {
-            var input1 = "set-alias -n \"alias Name\" \"queue-build 42\"";
-            var command1 = CommandParser.ParseCommand(input1);
+            var command = CommandParser.ParseCommand(string.Join(" ", args));
+            Console.WriteLine(string.Join("", command.Output));
 
-            var input2 = "set-alias -n \"alias Name\" -c \"queue-build 42\"";
-            var command2 = CommandParser.ParseCommand(input2);
+            //var input = "help build";
+            //var command = CommandParser.ParseCommand(input);
 
-            Console.ReadLine();
+            //Console.WriteLine(command.Output);
+
+            //var input1 = "set-alias -n \"alias Name\" \"queue-build 42\"";
+            //var command1 = CommandParser.ParseCommand(input1);
+
+            //var input2 = "set-alias -n \"alias Name\" -c \"queue-build 42\"";
+            //var command2 = CommandParser.ParseCommand(input2);
+
+            //Console.ReadLine();
         }
 
 
